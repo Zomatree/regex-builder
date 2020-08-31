a83377dd50ba9557126a8d0a6d5a987df4fccad3
zomatree <angelokontaxis@hotmail.com>
2020-08-31 20:42:19 +0000

    pypi stuff

--- build/lib/regex_builder/__init__.py
+++
@@ -1,6 +0,0 @@
-from .quantifiers import *
-from .constants import *
-from .section import *
-from .errors import *
-from .regex import *
-from .misc import *
--- build/lib/regex_builder/errors.py
+++
@@ -1,2 +0,0 @@
-class NotSection(Exception):
-    pass
